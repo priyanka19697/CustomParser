a25c9e5c4948785978d6ca1802883e813ba2f5d5
Priyanka <priyanka19697@gmail.com>
2023-07-27 14:02:43 +0200

    bubbling up common attributes

--- custom_parser/mapping_provider/MappingProvider.py
+++ custom_parser/mapping_provider/MappingProvider.py
@@ -1,8 +1,9 @@
+
 import csv
 from operator import itemgetter
 
-class MappingProvider:
 
+class MappingProvider:
     # takes mapping.csv and returns dicts for
     DELIMETER = "|"
 
@@ -12,46 +13,56 @@ class MappingProvider:
         self._createMappingDicts(mapping_file_path)
 
     def _createMappingDicts(self, file_path):
-        DESTINATION_TYPE_COLUMN = 'destination_type'
-        SOURCE_TYPE_COLUMN = 'source_type'
+        DESTINATION_TYPE_COLUMN = "destination_type"
+        SOURCE_TYPE_COLUMN = "source_type"
         try:
             with open(file_path) as csvfile:
                 reader = csv.DictReader(csvfile, delimiter=";")
 
                 for row in reader:
-                    self.supportedKeys.update(row[SOURCE_TYPE_COLUMN].split(self.DELIMETER))
-                    if row[DESTINATION_TYPE_COLUMN] not in self.destinationTypeToValueMapping:
-                        self.destinationTypeToValueMapping[row[DESTINATION_TYPE_COLUMN]] = {
-                            "source_type":  row[SOURCE_TYPE_COLUMN],
-                            "value_mappings": {
-                                row['source']: row['destination']
-                            }
-                    }
+                    self.supportedKeys.update(
+                        row[SOURCE_TYPE_COLUMN].split(self.DELIMETER)
+                    )
+                    if (
+                        row[DESTINATION_TYPE_COLUMN]
+                        not in self.destinationTypeToValueMapping
+                    ):
+                        self.destinationTypeToValueMapping[
+                            row[DESTINATION_TYPE_COLUMN]
+                        ] = {
+                            "source_type": row[SOURCE_TYPE_COLUMN],
+                            "value_mappings": {row["source"]: row["destination"]},
+                        }
                     else:
-                        self.destinationTypeToValueMapping[row[DESTINATION_TYPE_COLUMN]
-                                                                ]["value_mappings"][row['source']] = row['destination']
-                        
+                        self.destinationTypeToValueMapping[
+                            row[DESTINATION_TYPE_COLUMN]
+                        ]["value_mappings"][row["source"]] = row["destination"]
+
         except FileNotFoundError:
             print("File could not be found")
 
     def getSupportedKeys(self):
         return self.supportedKeys
-    
+
+
     def getAvailableDestinationKeys(self):
         return set(self.destinationTypeToValueMapping.keys())
 
-    def getSourceTypeForDestinationKey(self, destinationKey) -> list:
-        try:
-            return self.destinationTypeToValueMapping.get(destinationKey)
-        except KeyError as e:
-            raise KeyError(e) # TODO: Replace with custom error
-
     def getValue(self, destinationKey: str, row: dict):
-        
+
         result = self.destinationTypeToValueMapping[destinationKey]
-        source_type, value_mappings = itemgetter('source_type', 'value_mappings')(result)
+        source_type, value_mappings = itemgetter("source_type", "value_mappings")(
+            result
+        )
 
         source_key_list = source_type.split(self.DELIMETER)
         value_mappings_key = self.DELIMETER.join([row[key] for key in source_key_list])
 
-        return value_mappings[value_mappings_key]
+        try:
+            result_value = value_mappings[value_mappings_key]
+        except KeyError as e:
+            print("KeyError:", e)
+        
+        return result_value
+    
+
--- custom_parser/result_builder/ResultBuilder.py
+++ custom_parser/result_builder/ResultBuilder.py
@@ -1,29 +1,34 @@
 import json
+
 from ..mapping_provider import MappingProvider
 from collections import defaultdict
 import csv
 
+
 class Variation:
     def __init__(self) -> None:
         return
-    
+
     def __str__(self):
         return str(self.__dict__)
+
 
 class Article:
     def __init__(self, article_number) -> None:
         self.article_number = article_number
         self.variations = []
-    
+
     def __str__(self):
         return str(self.__dict__)
+
 
 class Catalog:
     def __init__(self) -> None:
         self.articles = []
-        
+
     def __str__(self):
         return str(self.__dict__)
+
 
 class ResultBuilder:
     def __init__(self, input_file, mapping_provider: MappingProvider) -> None:
@@ -40,11 +45,18 @@ class ResultBuilder:
                 continue
             else:
                 new_row[k] = v
-
+            
             for available_key in available_destination_keys:
-                new_row[available_key] = self.mapping_provider.getValue(available_key, row)
+                # import pdb;pdb.set_trace()
+                new_row[available_key] = self.mapping_provider.getValue(
+                    available_key, row
+                )
         return new_row
-    
+
+    def make_result(self):
+        self.bubble_up()
+        return json.dumps(self.catalog, default=lambda o: o.__dict__)
+
     def _create_initial_articles(self, input_file):
         with open(input_file) as csvfile:
             reader = csv.DictReader(csvfile, delimiter=";")
@@ -52,8 +64,8 @@ class ResultBuilder:
             # process each row
             articles_dict = defaultdict(list)
             for row in reader:
-                articles_dict[row['article_number']].append(self.process_row(row=row))
-            
+                articles_dict[row["article_number"]].append(self.process_row(row=row))
+
             articles = []
             for article_number, article_variations in articles_dict.items():
                 art = Article(article_number=article_number)
@@ -68,7 +80,80 @@ class ResultBuilder:
 
             self.catalog.articles = articles
 
-    def make_result(self):
-        json_data = json.dumps(self.catalog,  default=lambda o: o.__dict__)
-        with open('data/result.json', 'w') as json_file:
-            json_file.write(json_data)
+    def _find_common_attributes(self, objects: list) -> list:
+        # Takes in list of objects of same type, then returns
+        # list of attributes that were common across all the objects
+
+        # Parameters:
+        #   objects (list): list of objects
+
+        # Returns
+        #   list: list of attribute keys as strings
+        common_attributes = []
+        result = defaultdict(set)
+        for obj in objects:
+            for k, v in obj.__dict__.items():
+                # skip complex types
+                if type(v) == list:
+                    continue
+                result[k].add(v)
+
+        for attr, value_list in result.items():
+            if len(value_list) == 1:
+                common_attributes.append(attr)
+
+        return common_attributes
+
+    def _find_common_attributes2(self, objects: list) -> list:
+        # Takes in list of objects of same type, then returns
+        # list of attributes that were common across all the objects
+
+        # Parameters:
+        #   objects (list): list of objects
+
+        # Returns
+        #   list: list of attribute keys as strings
+        reference = objects[0]
+        attributes = reference.__dict__
+
+        common_attrs = []
+        for k, v in attributes.items():
+            flag = True
+            for obj in objects:
+                if getattr(obj, k) != v:
+                    flag = False
+            if flag:
+                common_attrs.append(k)
+
+        return common_attrs
+
+    def _bubble_up_common_to_parent(self, parent_obj, nested_objects):
+        # Takes in an object and a list of objects. From this list
+        # of objects it finds attributes which have common values
+        # (have same value in all the objects) then puts it in parent_obj
+        # and removes from nested objects
+        # NOTE: This mutates the parent object and nested_objects
+
+        # Parameters:
+        #   parent_obj: object
+        #   nested_objects: list of objects
+
+        # Returns
+        #   None
+
+        common_attributes = self._find_common_attributes2(nested_objects)
+
+        reference_obj = nested_objects[0]  # just to get values to bubble up
+
+        for attr in common_attributes:
+            setattr(parent_obj, attr, getattr(reference_obj, attr, None))  # move
+
+        for nested_obj in nested_objects:
+            for attr in common_attributes:
+                delattr(nested_obj, attr)  # delete from child objects
+
+    def bubble_up(self):
+        for article in self.catalog.articles:
+            self._bubble_up_common_to_parent(article, article.variations)
+
+        self._bubble_up_common_to_parent(self.catalog, self.catalog.articles)
